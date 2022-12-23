347113a83659f1a2442d80f8df9eb727faaafd57
LuisandLan <ghg00rus300@gmail.com>
2022-12-23 17:38:24 +0700

    Version 1.0

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.photogallery.api
+
+import android.content.ContentValues.TAG
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.annotation.WorkerThread
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest?method=flickr.interestingness.getList"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String):
+            Call<ResponseBody>
+
+    @GET("services/rest?\n" +
+            "method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String):Call<FlickrResponse>
+
+}
